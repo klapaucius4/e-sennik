796bcf2c593d0b4d90becba6029a5b1e40b30385
klapaucius4 <klapaucius.mk@gmail.com>
2020-07-31 14:27:22 +0200

    mods

--- footer.php
+++ footer.php
@@ -98,16 +98,21 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 	<!-- Footer section -->
 	<footer class="footer-section">
 		<div class="container">
-			<ul class="footer-menu">
-				<li><a href="">Home</a></li>
-				<li><a href="">About us</a></li>
-				<li><a href="">Services</a></li>
-				<li><a href="">News</a></li>
-				<li><a href="">Contact</a></li>
-			</ul>
-			<div class="copyright"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></div>
+			<?php
+			if(has_nav_menu('menu-1')):
+				wp_nav_menu(
+					array(
+						'menu'   => 'menu-1',
+						'container' => false,
+						'menu_class' => 'footer-menu'
+					)
+				);
+			endif;
+			?>
+			<div class="copyright">
+			<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+			Copyright &copy; 2014 - <script>document.write(new Date().getFullYear());</script> All rights reserved
+			<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></div>
 		</div>
 	</footer>
 	<!-- Footer top section end -->
