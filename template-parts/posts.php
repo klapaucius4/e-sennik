2f62fb837b03f2d6435c0f30538dba2e47b24104
klapaucius4 <klapaucius.mk@gmail.com>
2020-08-09 22:52:25 +0200

    mods

--- template-parts/posts.php
+++ template-parts/posts.php
@@ -5,7 +5,9 @@
         $frontpageId = get_option( 'page_on_front' );
         $blogId = get_option('page_for_posts');
 
-        $category = get_queried_object();
+        if(is_category()){
+            $category = get_queried_object();
+        }
 
         if(is_archive()){
             echo post_type_archive_title( '', false );
@@ -34,7 +36,7 @@
     <div class="container">
         <div class="section-title">
 			<img src="http://e-sennik2.localhost/wp-content/themes/e-sennik/img/section-title-icon-light.svg" alt="">
-            <?php if($category): var_dump($category); exit;
+            <?php if($category):
                 $parentCategory = get_term_by('id', $category->parent, $category->taxonomy);
                 if($parentCategory && $parentCategory->slug == 'sen'): ?>
                     <p><?= __('Sny na literę'); ?></p>
@@ -43,7 +45,7 @@
                     <h2><?= $category->name; ?></h2>
                 <?php endif; ?>
             <?php else: ?>
-                <h2>aaa<?= get_the_title(); ?></h2>
+                <h2><?= get_the_title(); ?></h2>
             <?php endif; ?>
 		</div>
         <div class="row">
