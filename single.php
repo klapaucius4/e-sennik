4c27bc407af883bcfe4fc5144ebe7622693beeac
klapaucius4 <klapaucius.mk@gmail.com>
2020-08-16 15:34:49 +0200

    mods

--- single.php
+++ single.php
@@ -33,8 +33,8 @@
                         </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-md-12 mb-4">
+                <div class="row mt-5">
+                    <div class="col-md-12 mb-3">
                         <h5><?= __('Komentarze'); ?></h5>
                     </div>
                     <div class="col-md-12">
