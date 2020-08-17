f91d25ef73357c4b8e3296884a1a0bb0ea7b52d8
klapaucius4 <klapaucius.mk@gmail.com>
2020-08-17 21:33:54 +0200

    mods

--- 404.php
+++ 404.php
@@ -7,8 +7,8 @@
             <div class="row justify-content-center">
                 <div class="col-md-12 text-center">
                     <span class="display-1 d-block">404</span>
-                    <div class="mb-4 lead">The page you are looking for was not found.</div>
-                    <a href="https://www.totoprayogo.com/#" class="btn btn-link">Back to Home</a>
+                    <div class="mb-4 lead"><?= __('Nie odnaleziono strony...') ?></div>
+                    <a href="<?= home_url(); ?>" class="btn btn-link"><?= __('Wróć do strony głównej'); ?></a>
                 </div>
             </div>
         </div>
